e454f12cab3bb2090abfaac17f4bacf78bdcde0a
Randrian <Randrian@localhost>
2017-02-24 14:33:20 +0100

    * use imageio for loading image to respect exif rotation

--- pylustration/drag_bib.py
+++ pylustration/drag_bib.py
@@ -2,6 +2,7 @@ from __future__ import division
 import matplotlib.pyplot as plt
 from matplotlib.text import Text
 import numpy as np
+import imageio
 
 
 def fig_text(x, y, text, unit="cm", *args, **kwargs):
@@ -38,7 +39,7 @@ def add_axes(dim, unit="cm", *args, **kwargs):
 
 
 def add_image(filename):
-    plt.imshow(plt.imread(filename))
+    plt.imshow(imageio.imread(filename))
     plt.xticks([])
     plt.yticks([])
 
