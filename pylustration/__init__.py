7d439531bbb51b5a4984cfbc2441b1742312d230
Randrian <Randrian@localhost>
2017-08-17 16:12:53 +0200

    + added mark_inset, VoronoiPlot, selectRectangle and update the saved block anywhere in the text where it is found

--- pylustration/__init__.py
+++ pylustration/__init__.py
@@ -1,1 +1,1 @@
-from .drag_bib import StartPylustration, fig_text, add_axes, add_image, moveArtist, despine, changeFigureSize
+from .drag_bib import StartPylustration, fig_text, add_axes, add_image, moveArtist, despine, changeFigureSize, mark_inset, VoronoiPlot
