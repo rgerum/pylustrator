5c3761662427faee6f4e21663f957dd6ac5a6095
rgerum <richard.gerum@yahoo.de>
2018-01-21 13:09:09 +0100

    * moved stuff to helper_functions, added legend dragging

--- pylustration/drag_bib.py
+++ pylustration/drag_bib.py
@@ -1,214 +1,11 @@
-from __future__ import division
+from __future__ import division, print_function
+import numpy as np
+import traceback
 import matplotlib.pyplot as plt
 from matplotlib.text import Text
-import numpy as np
-import imageio
-import traceback
-
-
-def fig_text(x, y, text, unit="cm", *args, **kwargs):
-    """
-    add a text to the figure positioned in cm
-    """
-    fig = plt.gcf()
-    if unit == "cm":
-        x = x / 2.54 / fig.get_size_inches()[0]
-        y = y / 2.54 / fig.get_size_inches()[1]
-    if x < 0:
-        x += 1
-    if y < 0:
-        y += 1
-    return fig.text(x, y, text, picker=True, *args, **kwargs)
-
-
-def add_axes(dim, unit="cm", *args, **kwargs):
-    """
-    add an axes with dimensions specified in cm
-    """
-    fig = plt.gcf()
-    x, y, w, h = dim
-    if unit == "cm":
-        x = x / 2.54 / fig.get_size_inches()[0]
-        y = y / 2.54 / fig.get_size_inches()[1]
-        w = w / 2.54 / fig.get_size_inches()[0]
-        h = h / 2.54 / fig.get_size_inches()[1]
-    if x < 0:
-        x += 1
-    if y < 0:
-        y += 1
-    return plt.axes([x, y, w, h], *args, **kwargs)
-
-
-def add_image(filename):
-    plt.imshow(imageio.imread(filename))
-    plt.xticks([])
-    plt.yticks([])
-
-
-def changeFigureSize(w, h, cut_from_top=False, cut_from_left=False):
-    oldw, oldh = plt.gcf().get_size_inches()
-    fx = oldw / w
-    fy = oldh / h
-    for axe in plt.gcf().axes:
-        box = axe.get_position()
-        if cut_from_top:
-            axe.set_position([box.x0 * fx, box.y0 * fy, (box.x1 - box.x0) * fx, (box.y1 - box.y0) * fy])
-        else:
-            if cut_from_left:
-                axe.set_position(
-                    [1 - (1 - box.x0) * fx, 1 - (1 - box.y0) * fy, (box.x1 - box.x0) * fx, (box.y1 - box.y0) * fy])
-            else:
-                axe.set_position([box.x0 * fx, 1 - (1 - box.y0) * fy, (box.x1 - box.x0) * fx, (box.y1 - box.y0) * fy])
-    for text in plt.gcf().texts:
-        x0, y0 = text.get_position()
-        if cut_from_top:
-            text.set_position([x0 * fx, y0 * fy])
-        else:
-            if cut_from_left:
-                text.set_position([1 - (1 - x0) * fx, 1 - (1 - y0) * fy])
-            else:
-                text.set_position([x0 * fx, 1 - (1 - y0) * fy])
-    plt.gcf().set_size_inches(w, h, forward=True)
-
-
-def mark_inset(parent_axes, inset_axes, loc1=1, loc2=2, **kwargs):
-    from mpl_toolkits.axes_grid1.inset_locator import TransformedBbox, BboxPatch, BboxConnector
-    try:
-        loc1a, loc1b = loc1
-    except:
-        loc1a = loc1
-        loc1b = loc1
-    try:
-        loc2a, loc2b = loc2
-    except:
-        loc2a = loc2
-        loc2b = loc2
-    rect = TransformedBbox(inset_axes.viewLim, parent_axes.transData)
-
-    pp = BboxPatch(rect, fill=False, **kwargs)
-    parent_axes.add_patch(pp)
-    pp.set_clip_on(False)
-
-    p1 = BboxConnector(inset_axes.bbox, rect, loc1=loc1a, loc2=loc1b, **kwargs)
-    inset_axes.add_patch(p1)
-    p1.set_clip_on(False)
-    p2 = BboxConnector(inset_axes.bbox, rect, loc1=loc2a, loc2=loc2b, **kwargs)
-    inset_axes.add_patch(p2)
-    p2.set_clip_on(False)
-
-    return pp, p1, p2
-
-
-def draw_from_point_to_bbox(parent_axes, insert_axes, point, loc=1, **kwargs):
-    from mpl_toolkits.axes_grid1.inset_locator import TransformedBbox, BboxConnector, Bbox
-    rect = TransformedBbox(Bbox([point, point]), parent_axes.transData)
-    # rect = TransformedBbox(Bbox([[1, 0], [1, 0]]), parent_axes.transData)
-    p1 = BboxConnector(rect, insert_axes.bbox, loc, **kwargs)
-    parent_axes.add_patch(p1)
-    p1.set_clip_on(False)
-    return p1
-
-
-def draw_from_point_to_point(parent_axes, insert_axes, point1, point2, **kwargs):
-    from mpl_toolkits.axes_grid1.inset_locator import TransformedBbox, BboxConnector, Bbox
-    rect = TransformedBbox(Bbox([point1, point1]), parent_axes.transData)
-    rect2 = TransformedBbox(Bbox([point2, point2]), insert_axes.transData)
-    # rect = TransformedBbox(Bbox([[1, 0], [1, 0]]), parent_axes.transData)
-    loc = 1
-    p1 = BboxConnector(rect, rect2, loc, **kwargs)
-    parent_axes.add_patch(p1)
-    p1.set_clip_on(False)
-    return p1
-
-
-def mark_inset_pos(parent_axes, inset_axes, loc1, loc2, point, **kwargs):
-    kwargs["lw"] = 0.8
-    ax_new = plt.axes(inset_axes.get_position())
-    ax_new.set_xlim(point[0], point[0])
-    ax_new.set_ylim(point[1], point[1])
-    mark_inset(parent_axes, ax_new, loc1, loc2, **kwargs)
-    plt.xticks([])
-    plt.yticks([])
-    ax_new.set_zorder(inset_axes.get_zorder() - 1)
-
-
-def VoronoiPlot(points, values, vmin=None, vmax=None, cmap=None):
-    from matplotlib.patches import Polygon
-    from matplotlib.collections import PatchCollection
-    from scipy.spatial import Voronoi, voronoi_plot_2d
-    from matplotlib import cm
-
-    if cmap is None:
-        cmap = cm.get_cmap('viridis')
-
-    vor = Voronoi(points)
-
-    # %%
-    patches = []
-    dist_list = []
-    excluded_indices = []
-    for index, p in enumerate(points):
-        # print(index)
-        reg = vor.regions[vor.point_region[index]]
-        if -1 in reg:
-            # plt.plot(p[0], p[1], 'ok', alpha=0.3, ms=1)
-            excluded_indices.append(index)
-            continue
-        distances = np.linalg.norm(np.array([vor.vertices[i] for i in reg]) - p, axis=1)
-        if np.max(distances) > 2:
-            # plt.plot(p[0], p[1], 'ok', alpha=0.3, ms=1)
-            excluded_indices.append(index)
-            continue
-        region = np.array([vor.vertices[i] for i in reg])
-        polygon = Polygon(region, True)
-        patches.append(polygon)
-        dists = values[index]
-        dist_list.append(dists)
-        # plt.plot(p[0], p[1], 'ok', alpha=0.3, ms=1)
-
-    p = PatchCollection(patches, cmap=cmap)
-    p.set_clim([vmin, vmax])
-    p.set_array(np.array(dist_list))
-    p.set_linewidth(10)
-
-    plt.gca().add_collection(p)
-    plt.xticks([])
-    plt.yticks([])
-    return p, excluded_indices
-
-
-def selectRectangle(axes=None):
-    if axes is None:
-        axes = plt.gca()
-
-    def onselect(eclick, erelease):
-        'eclick and erelease are matplotlib events at press and release'
-        print(' startposition : (%f, %f)' % (eclick.xdata, eclick.ydata))
-        print(' endposition   : (%f, %f)' % (erelease.xdata, erelease.ydata))
-        print(' used button   : ', eclick.button)
-
-    from matplotlib.widgets import RectangleSelector
-    rect_selector = RectangleSelector(axes, onselect)
-    return rect_selector
-
-
-def despine(ax=None, complete=False):
-    if not ax:
-        ax = plt.gca()
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    if complete:
-        ax.spines['left'].set_visible(False)
-        ax.spines['bottom'].set_visible(False)
-        ax.set_xticks([])
-        ax.set_yticks([])
-    else:
-        # Only show ticks on the left and bottom spines
-        ax.yaxis.set_ticks_position('left')
-        ax.xaxis.set_ticks_position('bottom')
-
-""" drag stuff """
-
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle, Ellipse
+from matplotlib.legend import Legend
 
 class FigureDragger:
     last_picked = False
@@ -272,6 +69,9 @@ class FigureDragger:
         # make all the subplots pickable
         for axes in self.fig.axes:
             axes.set_picker(True)
+            leg = axes.get_legend()
+            if leg:
+                leg.draggable(use_blit=True)
 
     def draw(self):
         # only draw if the canvas is not already drawing
@@ -322,20 +122,36 @@ class FigureDragger:
             self.draw()
 
     def key_press_event(self, event):
-        global last_axes, nosnap
-        global stack_position
         # space: print code to restore current configuration
         if event.key == ' ':
+            # print comment that the block starts
             save_text = "#% start: automatic generated code from pylustration\n"
-            save_text += "plt.gcf().set_size_inches(%f/2.54, %f/2.54, forward=True)\n" % (
+            # get the figure by its name
+            save_text += "fig = plt.figure(%s)\n" % self.fig.number
+            # set the size of the figure
+            save_text += "fig.set_size_inches(%f/2.54, %f/2.54, forward=True)\n" % (
                 (self.fig.get_size_inches()[0] - self.inch_offset[0]) * 2.54, (self.fig.get_size_inches()[1] - self.inch_offset[1]) * 2.54)
+            # iterate over all axes
             for index, ax in enumerate(self.fig.axes):
+                # get the position of the axes
                 pos = ax.get_position()
-                save_text += "plt.gcf().axes[%d].set_position([%f, %f, %f, %f])\n" % (
+                # and set the position
+                save_text += "fig.axes[%d].set_position([%f, %f, %f, %f])\n" % (
                     index, pos.x0, pos.y0, pos.width, pos.height)
+                # set the zorder of the figure
                 if ax.get_zorder() != 0:
-                    save_text += "plt.gcf().axes[%d].set_zorder(%d)\n" % (index, ax.get_zorder())
+                    save_text += "fig.axes[%d].set_zorder(%d)\n" % (index, ax.get_zorder())
+
                 for index2, artist in enumerate(ax.get_children()):
+                    # if the artist is a legend
+                    if isinstance(artist, Legend):
+                        # if the location is a tuple
+                        loc = artist._get_loc()
+                        if isinstance(loc, tuple):
+                            # set the position of the legend
+                            save_text += "fig.axes[%d].get_legend()._set_loc(%s)\n" % (index, artist._get_loc())
+                        continue
+                    print(index2, artist)
                     if artist.pickable():
                         try:
                             pos0 = artist.original_pos
@@ -347,7 +163,7 @@ class FigureDragger:
             for index, txt in enumerate(self.fig.texts):
                 if txt.pickable():
                     pos = txt.get_position()
-                    save_text += "plt.gcf().texts[%d].set_position([%f, %f])\n" % (index, pos[0], pos[1])
+                    save_text += "fig.texts[%d].set_position([%f, %f])\n" % (index, pos[0], pos[1])
             save_text += "#% end: automatic generated code from pylustration"
             print(save_text)
             insertTextToFile(save_text, self.stack_position)
@@ -412,12 +228,11 @@ class FigureDragger:
     def on_pick_event(self, event):
         " Store which text object was picked and were the pick event occurs."
 
-        print("picker", event)
-
         self.last_picked = True
         artist = event.artist
+        print("picker", event, artist)
 
-        if isinstance(event.artist, Text):
+        if isinstance(artist, Text):
             if self.active_object is not None:
                 self.deselectArtist()
             self.active_object = event.artist
@@ -433,8 +248,15 @@ class FigureDragger:
                 self.drag_object = self.grabbers[-1]
                 self.grabbers[-1].clickedEvent(event)
             except AttributeError:
-                self.drag_object = artist
-                artist.clickedEvent(event)
+                if isinstance(artist, Grabber):
+                    self.drag_object = artist
+                    artist.clickedEvent(event)
+                else:
+                    if self.drag_object:
+                        self.drag_object.releasedEvent(event)
+                        self.drag_object = None
+                    if self.active_object is not None:
+                        self.deselectArtist()
 
         return True
 
@@ -530,11 +352,6 @@ def insertTextToFile(text, stack_pos):
                 fp1.write(line)
     print("Save to", stack_pos.filename, "line", stack_pos.lineno)
 
-
-
-
-from matplotlib.lines import Line2D
-from matplotlib.patches import Rectangle, Ellipse
 
 class Snap(Line2D):
     def __init__(self, x, y, draw_x, draw_y, transform=-1):
