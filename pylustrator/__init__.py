665a98e9f797b849e2d6dfdf0e41f1f788e1bd06
rgerum <richard.gerum@yahoo.de>
2018-08-02 10:58:06 +0200

    * __init__.py: added version number

--- pylustrator/__init__.py
+++ pylustrator/__init__.py
@@ -3,3 +3,5 @@ from .QtGuiDrag import initialize as start
 from .helper_functions import fig_text, add_axes, add_image, despine, changeFigureSize, mark_inset, VoronoiPlot, selectRectangle, mark_inset_pos, draw_from_point_to_bbox, draw_from_point_to_point, loadFigureFromFile, add_letter, add_letters
 from .QtGui import initialize as StartColorChooser
 from .lab_colormap import LabColormap
+
+__version__ = '0.8.0'
