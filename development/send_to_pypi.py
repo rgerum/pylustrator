cc635e16a25db39f5ffd364269a1815dfdcc6d8e
rgerum <richard.gerum@yahoo.de>
2019-03-05 13:06:40 +0100

    ! fixed send to pypi

--- development/send_to_pypi.py
+++ development/send_to_pypi.py
@@ -5,15 +5,41 @@ from __future__ import print_function, division
 import os
 import sys
 
-from release_tools import check_for_uncommited_changes, get_current_version
+current_folder = os.getcwd()
 
-# get the current version (also changes the directory to the pylustrator directory)
-current_version = get_current_version()
+try:
+    # go to parent folder
+    os.chdir(os.path.join(os.path.dirname(__file__), ".."))
 
-# check for uncommitted changes
-check_for_uncommited_changes()
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "pylustrator"))
+    import pylustrator
 
-# upload to pypi
-os.system("pip install twine")
-os.system("python setup.py sdist")
-os.system("twine upload dist/pylustrator-%s.tar.gz" % (current_version, ))
+    current_version = pylustrator.__version__
+
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    parser.add_option("-u", "--username", action="store", dest="username")
+    parser.add_option("-p", "--password", action="store", dest="password")
+    (options, args) = parser.parse_args()
+
+    # upgrade twine (used for uploading)
+    os.system("pip install twine --upgrade")
+
+    # pack clickpoints
+    os.system("python setup.py sdist")
+
+    # the command
+    command_string = "twine upload dist/pylustrator-%s.tar.gz" % current_version
+    # optionally add the username
+    if options.username:
+        command_string += " --username %s" % options.username
+    # optionally add the password
+    if options.password:
+        command_string += " --password %s" % options.password
+    # print the command string
+    print(command_string)
+    # and execute it
+    os.system(command_string)
+finally:
+    os.chdir(current_folder)
