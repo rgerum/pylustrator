2a9574f40c656a367c81366e922423cca353147c
Richard Gerum <richard.gerum@yahoo.de>
2019-08-29 10:35:59 +0200

    parse_svg: improved svg support with more stroke properties

--- pylustrator/parse_svg.py
+++ pylustrator/parse_svg.py
@@ -4,6 +4,7 @@ import matplotlib.patches as patches
 import matplotlib.transforms as transforms
 import sys
 import numpy as np
+import matplotlib.text
 
 def deform(base_trans, x, y, sx=0, sy=0):
     return transforms.Affine2D([[x, sx, 0], [sy, y, 0], [0, 0, 1]]) + base_trans
@@ -32,45 +33,77 @@ def parseTransformation(trans, base_trans):
 def parse_style(style, patch):
     for element in style.split(";"):
         key, value = element.split(":", 1)
-        if key == "opacity":
-            patch.set_alpha(float(value))
-        elif key == "fill":
-            try:
-                patch.set_facecolor(value)
-            except AttributeError:
-                patch.set_color(value)
-        elif key == "stroke":
-            try:
-                patch.set_edgecolor(value)
-            except AttributeError:
-                pass
-                #patch.set_color(value)
-        elif key == "stroke-width":
-            try:
+        try:
+            if key == "opacity":
+                patch.set_alpha(float(value))
+            elif key == "fill":
+                patch.svg_fill = value
+                try:
+                    patch.set_facecolor(value)
+                except AttributeError:
+                    patch.set_color(value)
+            elif key == "fill-opacity":
+                if getattr(patch, "svg_fill", "none") != "none":
+                    r, g, b, a = patch.get_facecolor()
+                    patch.set_facecolor((r, g, b, float(value)))
+            elif key == "stroke":
+                patch.svg_stroke = value
+                try:
+                    patch.set_edgecolor(value)
+                except AttributeError:
+                    pass
+                    #patch.set_color(value)
+            elif key == "stroke-opacity":
+                if getattr(patch, "svg_stroke", "none")[0] == "#":
+                    r, g, b, a = patch.get_edgecolor()
+                    patch.set_edgecolor((r, g, b, float(value)))
+            elif key == "stroke-dasharray":
+                if value != "none":
+                    offset = 0
+                    if isinstance(patch.get_linestyle(), tuple):
+                        offset, dashes = patch.get_linestyle()
+                    patch.set_linestyle((offset, [float(s)*4 for s in value.split(",")]))
+            elif key == "stroke-dashoffset":
+                dashes = [1, 0]
+                if isinstance(patch.get_linestyle(), tuple):
+                    offset, dashes = patch.get_linestyle()
+                patch.set_linestyle((float(value)*4, dashes))
+            elif key == "stroke-linecap":
+                if value == "square":
+                    value = "projecting"
+                patch.set_capstyle(value)
+            elif key == "stroke-linejoin":
+                patch.set_joinstyle(value)
+            elif key == "stroke-miterlimit":
+                pass  # unfortunately we cannot implement this in matplotlib
+            elif key == "stroke-width":
+                try:
+                    if value.endswith("px"):
+                        patch.set_linewidth(float(value[:-2]) * 100)
+                    else:
+                        patch.set_linewidth(float(value) * 100)
+                    pass
+                except:
+                    pass
+            elif key == "stroke-linecap":
+                try:
+                    patch.set_dash_capstyle(value)
+                    patch.set_solid_capstyle(value)
+                except AttributeError:
+                    pass
+            elif key == "font-size":
                 if value.endswith("px"):
-                    patch.set_linewidth(float(value[:-2]) * 100)
+                    patch.set_fontsize(float(value[:-2]) * 50)
                 else:
-                    patch.set_linewidth(float(value) * 100)
-                pass
-            except:
-                pass
-        elif key == "stroke-linecap":
-            try:
-                patch.set_dash_capstyle(value)
-                patch.set_solid_capstyle(value)
-            except AttributeError:
-                pass
-        elif key == "font-size":
-            if value.endswith("px"):
-                patch.set_fontsize(float(value[:-2]) * 50)
+                    patch.set_fontsize(float(value) * 100)
+            elif key == "font-weight":
+                patch.set_fontweight(value)
+            elif key == "font-style":
+                patch.set_fontstyle(value)
             else:
-                patch.set_fontsize(float(value) * 100)
-        elif key == "font-weight":
-            patch.set_fontweight(value)
-        elif key == "font-style":
-            patch.set_fontstyle(value)
-        else:
-            print("ERROR: unknown style key", key, file=sys.stderr)
+                print("ERROR: unknown style key", key, file=sys.stderr)
+        except ValueError:
+            print("ERROR: could not set style", element, file=sys.stderr)
 
 def plt_draw_rect(node, trans):
     trans = parseTransformation(node.getAttribute("transform"), trans)
@@ -140,19 +173,37 @@ def plt_draw_path(node, trans):
             part = part.lower()
             if part == "m":
                 last_command = mpath.Path.MOVETO
+            elif part == "v":
+                last_command = "v"#mpath.Path.MOVETO
+            elif part == "h":
+                last_command = "h"
             elif part == "l":
                 last_command = mpath.Path.LINETO
             elif part == "c":
                 last_command = mpath.Path.CURVE4
+            elif part == "z":
+                last_command = mpath.Path.CLOSEPOLY
+                verts.append(verts[0])
+                codes.append(mpath.Path.CLOSEPOLY)
             else:
                 raise ValueError("Unknown command", last_command)
             continue
-        x, y = [float(s) for s in part.split(",")]
+        if last_command == "v":
+            x = float(part)
+            y = 0
+        elif last_command == "h":
+            x = 0
+            y = float(part)
+        else:
+            x, y = [float(s) for s in part.split(",")]
         if last_command_relative is True:
             x += last_x
             y += last_y
         verts.append([x, y])
-        codes.append(last_command)
+        if last_command == "v" or last_command == "h":
+            codes.append(mpath.Path.LINETO)
+        else:
+            codes.append(last_command)
         if last_command == mpath.Path.MOVETO:
             last_command = mpath.Path.LINETO
         if last_command == mpath.Path.CURVE4:
@@ -205,6 +256,12 @@ def svgread(filename):
                 parseGroup(node, trans)
             elif node.tagName == "text":
                 plt_draw_text(node, trans)
+            elif node.tagName == "defs":
+                pass  # currently ignored might be used for example for gradient definitions
+            elif node.tagName == "sodipodi:namedview":
+                pass  # used for some inkscape metadata
+            elif node.tagName == "metadata":
+                pass  # we do not have to draw metadata
             else:
                 print("Unknown tag", node.tagName, file=sys.stderr)
 
