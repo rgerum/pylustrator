d02d4c131f2f0d8b8998bfc5aee8eb4fb6119fcd
Richard Gerum <richard.gerum+dev@protonmail.com>
2021-02-12 14:33:50 +0100

    fixed version number

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except FileNotFoundError:
     pass
 
 setup(name='pylustrator',
-      version="-v",
+      version="1.1.0",
       description='Adds interactivity to arrange panels in matplotlib',
       long_description=long_description,
       url='https://bitbucket.org/fabry_biophysics/pylustrator',
