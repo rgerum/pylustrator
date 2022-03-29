ff80f3e998a8972828dc214730e94c01f50545c6
rgerum <14153051+rgerum@users.noreply.github.com>
2022-03-29 14:19:27 -0400

    set version to v1.1.2

--- pylustrator/__init__.py
+++ pylustrator/__init__.py
@@ -25,4 +25,4 @@ from .QtGui import initialize as StartColorChooser
 from .lab_colormap import LabColormap
 from .helper_functions import loadFigureFromFile as load
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
