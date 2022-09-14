dd80530fc877f7e9cf9b71b656d1053f97d71184
JScheffer <joostscheffer@protonmail.com>
2022-09-14 09:25:28 +0200

    Make mpl ToolBar drag, pan and zoom exclusive and fix checkbox not working
    Before you could enable and disable drag, pan and zoom individually and you could select multiple at the same time. When selecting multiple the checkboxes would not update, the user can not know which tools are currently selected.
    
    Since I do not see a purpose of using multiple tools at the same time I made them exclusive, you can only use 1 at a time. Also the checkboxes now work as intended

--- pylustrator/QComplexWidgets.py
+++ pylustrator/QComplexWidgets.py
@@ -1405,18 +1405,31 @@ class ToolBar(QtWidgets.QToolBar):
 
         self._actions = self.navi_toolbar._actions
         self._actions["home"] = self.addAction(self.navi_toolbar._icon("home.png"), "", self.navi_toolbar.home)
+
         self._actions["back"] = self.addAction(self.navi_toolbar._icon("back.png"), "", self.navi_toolbar.back)
+
         self._actions["forward"] = self.addAction(self.navi_toolbar._icon("forward.png"), "", self.navi_toolbar.forward)
         self.addSeparator()
+
+        # the action group makes the actions exclusive, you
+        # can't use 2 at the same time
+        action_group = QtWidgets.QActionGroup(self)
+
         self._actions["drag"] = self.addAction(self.icon("arrow.png"), "", self.setSelect)
         self._actions["drag"].setCheckable(True)
+        self._actions["drag"].setActionGroup(action_group)
+
         self._actions["pan"] = self.addAction(self.navi_toolbar._icon("move.png"), "", self.setPan)
         self._actions["pan"].setCheckable(True)
+        self._actions["pan"].setActionGroup(action_group)
+
         self._actions["zoom"] = self.addAction(self.navi_toolbar._icon("zoom_to_rect.png"), "", self.setZoom)
         self._actions["zoom"].setCheckable(True)
+        self._actions["zoom"].setActionGroup(action_group)
 
         self.navi_toolbar._active = 'DRAG'
-        self.checkActive()
+        self._actions['drag'].setChecked(True)
+        self.prev_active = 'DRAG'
 
     def icon(self, name: str):
         """ get an icon with the given filename """
@@ -1431,30 +1444,33 @@ class ToolBar(QtWidgets.QToolBar):
 
     def setSelect(self):
         """ select the pylustrator selection and drag tool """
-        if self.navi_toolbar._active == 'PAN':
-            self.navi_toolbar.pan()
-        if self.navi_toolbar._active == 'ZOOM':
-            self.navi_toolbar.zoom()
         self.fig.figure_dragger.activate()
+
+        if self.prev_active=="PAN":
+            self.navi_toolbar.pan()
+        elif self.prev_active=="ZOOM":
+            self.navi_toolbar.zoom()
+
+        self.prev_active = 'DRAG'
+
         self.navi_toolbar._active = 'DRAG'
-        self.checkActive()
 
     def setPan(self):
         """ select the mpl pan tool """
-        self.fig.figure_dragger.deactivate()
+        if self.prev_active == "DRAG":
+            self.fig.figure_dragger.deactivate()
+
         if self.navi_toolbar._active != 'PAN':
             self.navi_toolbar.pan()
-        self.checkActive()
+
+        self.prev_active = 'PAN'
 
     def setZoom(self):
         """ select the mpl zoom tool """
-        self.fig.figure_dragger.deactivate()
+        if self.prev_active == "DRAG":
+            self.fig.figure_dragger.deactivate()
+
         if self.navi_toolbar._active != 'ZOOM':
             self.navi_toolbar.zoom()
-        self.checkActive()
 
-    def checkActive(self):
-        """ check which buttons are active """
-        self._actions['drag'].setChecked(self.navi_toolbar._active == 'DRAG')
-        self._actions['pan'].setChecked(self.navi_toolbar._active == 'PAN')
-        self._actions['zoom'].setChecked(self.navi_toolbar._active == 'ZOOM')
+        self.prev_active = 'ZOOM'
