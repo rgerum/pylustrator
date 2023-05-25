ef0a6473a6b6f360697e31bfd22f27f6c0e44d90
Mark Mayo <mark@there.co.nz>
2023-05-26 11:39:31 +1200

    removed u-prefix
    The u prefix for strings is no longer necessary in Python >=3.0

--- docs/conf.py
+++ docs/conf.py
@@ -80,9 +80,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pylustrator'
-copyright = u'2018, Richard Gerum'
-author = u'Richard Gerum'
+project = 'pylustrator'
+copyright = '2018, Richard Gerum'
+author = 'Richard Gerum'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -261,8 +261,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'pylustrator.tex', u'pylustrator Documentation',
-   u'Richard Gerum', 'manual'),
+  (master_doc, 'pylustrator.tex', 'pylustrator Documentation',
+   'Richard Gerum', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -291,7 +291,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pylustrator', u'Pylustrator Documentation',
+    (master_doc, 'pylustrator', 'Pylustrator Documentation',
      [author], 1)
 ]
 
@@ -305,7 +305,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'pylustrator', u'Pylustrator Documentation',
+  (master_doc, 'pylustrator', 'Pylustrator Documentation',
    author, 'pylustrator', 'One line description of project.',
    'Miscellaneous'),
 ]
